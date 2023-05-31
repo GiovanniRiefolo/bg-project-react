77101b0d4c0a2334c18c3ffc03a1da5283b07b57
Giovanni Riefolo <giovanniriefolo@MacBook-Air-di-Giovanni.local>
2023-05-31 09:02:24 +0200

    wip: infinite scroll

--- src/components/post_list.js
+++ src/components/post_list.js
@@ -1,27 +1,62 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Counter from "./counter";
+import loadingSvg from "../images/loading.svg";
 
 import "../styles/post_list.scss";
 
 export default function PostList() {
   const [posts, setPosts] = useState([]);
   const [noposts, setNoPosts] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // TODO: create a temp variable to get the current max index of posts
+  // then use it to slice the posts (prevIndex, nextIndex). Then concatenate
+  // the array (or push) to the current posts array.
+
+  const [currentPosts, setCurrentPosts] = useState(6);
 
   useEffect(() => {
-    fetch("http://localhost:3030/posts", {
-      method: "GET",
-    })
+    fetchPosts(0, currentPosts);
+
+    let observer = new IntersectionObserver(loadMorePosts, {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1.0,
+    });
+    
+    observer.observe(document.querySelector("#postListEnd"));
+
+  }, []);
+
+  const fetchPosts = (start, end) => {
+    setIsLoading(true);
+    const randomTime = Math.floor(Math.random() * 2000) + 1000;
+
+    fetch("http://localhost:3030/posts", { method: "GET" })
       .then((response) => response.json())
       .then((data) => {
-        setPosts(data.items);
+        setTimeout(() => {
+          setPosts(data.items.slice(start, end));
+          setIsLoading(false);
+        }, randomTime);
       })
       .catch((error) => {
         setNoPosts(true);
       });
-  }, []);
+  };
+
+  const loadMorePosts = (entries) => {
+    
+    let tempIndex
+    if(entries[0].isIntersecting && currentPosts < posts.length){
+      tempIndex = currentPosts
+      setCurrentPosts(currentPosts + 6)
+    }
+    fetchPosts(tempIndex, currentPosts)
+  };
 
   const formatDate = (value) => {
     let _date = new Date(value);
@@ -31,6 +66,15 @@ export default function PostList() {
       day: "numeric",
     });
   };
+
+  if (isLoading) {
+    return (
+      <>
+        <img src={loadingSvg} alt="" width="32" />
+        <p>Caricamento post...</p>
+      </>
+    );
+  }
 
   if (noposts) {
     return (
@@ -57,7 +101,7 @@ export default function PostList() {
                     data-comments={post.total_comments}
                     data-time-to-read={post.minutes_to_read}
                   />
-                  <img src={post.image} alt="" width="410" loading="lazy"/>
+                  <img src={post.image} alt="" width="410" loading="lazy" />
                   <h2>{post.title}</h2>
                 </figure>
                 <div className="author">
@@ -67,7 +111,12 @@ export default function PostList() {
                     className="author__avatar"
                   >
                     <figure>
-                      <img src={post.author.avatar} alt="" width="40" loading="lazy" />
+                      <img
+                        src={post.author.avatar}
+                        alt=""
+                        width="40"
+                        loading="lazy"
+                      />
                     </figure>
                     <span>{post.author.name}</span>
                   </a>
@@ -77,6 +126,7 @@ export default function PostList() {
               </article>
             </Col>
           ))}
+          <div id="postListEnd"></div>
         </Row>
       </Container>
     </>
